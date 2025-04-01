c598614776c80a46e89bb0301e76dce46198cbda
Dimas Maulana Ahmad <97804178+dimasmaulana-ach@users.noreply.github.com>
2025-04-01 12:46:49 +0700

    update auth request

--- src/config/api/auth.ts
+++ src/config/api/auth.ts
@@ -13,5 +13,16 @@ auth.interceptors.request.use((config) => {
   config.headers["Content-Type"] = "application/json";
   return config;
 });
+auth.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      useAuthStore.getState().clearUsers();
+      window.location.href = "/login";
+    }
+
+    return Promise.reject(error);
+  }
+);
 
 export default auth;
